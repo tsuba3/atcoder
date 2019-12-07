ed26a496e939e039ff82861e9f8da4d6912dd411
Tsubasa Munekata <tsubasa.afe@gmail.com>
2019-12-08 03:23:39 +0900

    進捗

--- abc146/f.cpp
+++ abc146/f.cpp
@@ -134,12 +134,171 @@ public:
 
 // End of template.
 
+template<typename T, T f_(T const, T const)>
+class Monoid {
+public:
+    using type = T;
+
+    static T f(T const a, T const b) {
+        return f_(a, b);
+    }
+
+    static const T unit;
+
+    template<typename I>
+    static T concat(I const& begin, I const& end) {
+        return accumulate(begin, end, unit, f);
+    }
+
+    static T pow(T base, int exp) {
+        T res = unit;
+        while (exp > 0) {
+            if (exp & 1) res = f(base, res);
+            base = f(base, base);
+            exp >>= 1;
+        }
+        return res;
+    }
+};
+
+template<typename M, typename Op = M, typename M::type f(typename M::type const, typename Op::type const) = M::f>
+class LazySegmentTree {
+public:
+    using MT = typename M::type;
+    using OpT = typename Op::type;
+    LazySegmentTree(const int n) {
+        depth = int (ceil(log2(n)));
+        this->n = 1 << depth;
+        v.resize(2 * this->n, M::unit);
+        lazy.resize(2 * this->n, Op::unit);
+    }
+    void set(int i, MT const m) {
+        i += n;
+        v[i] = m;
+        re_calc(i, i + 1);
+    }
+    void set(int l, int r, MT const m) {
+        l += n; r += n;
+        for (int i = l; i < r; ++i) v[i] = m;
+        re_calc(l, r);
+    }
+    template<typename I>
+    void set(int l, int r, I it) {
+        l += n; r += n;
+        for (int i = l; i < r; ++i) v[i] = *it++;
+        re_calc(l, r);
+    }
+    MT get(int const i) {
+        return v[n + i];
+    }
+    MT get(int l, int r) {
+        l += n; r += n;
+        int d = 0;
+        MT res = M::unit;
+        while (l < (r >> d)) {
+            if (l & 1) { // l is right side of parent or root
+                res = M::f(res, v[l]);
+                l += 1;
+            }
+            l /= 2;
+            d++;
+        }
+        while (d >= 0) {
+            l = 2 * l;
+            d--;
+            if (l < (r >> d)) {
+                res = M::f(res, v[l]);
+                l += 1;
+            }
+        }
+        return res;
+    }
+    /// 全区間取得 O(1)
+    MT get() const {
+        if (lazy[1] != Op::unit) {
+            lazy[2] = Op::f(lazy[2], lazy[1]);
+            lazy[3] = Op::f(lazy[3], lazy[1]);
+            v[1] = f(v[1], Op::pow(lazy[1], depth + 1));
+            lazy[1] = Op::unit;
+        }
+        return v[1];
+    }
+    /// 点演算 O(1)
+    void update(int const i, OpT const op) {
+        lazy[n + i] = Op::f(lazy[n + i], op);
+    }
+    /// 区間演算 O(log(r - l))
+    void update(int l, int r, OpT const op) {
+        l += n; r += n;
+        int d = 0;
+        while (l < (r >> d)) {
+            if (l & 1) { // l is right side of parent or root
+                lazy[l] = Op::f(lazy[l], op);
+                l += 1;
+            }
+            l /= 2;
+            d++;
+        }
+        while (d >= 0) {
+            l = 2 * l;
+            d--;
+            if (l < (r >> d)) {
+                lazy[l] = Op::f(lazy[l], op);
+                l += 1;
+            }
+        }
+    }
+    /// 全区間演算 O(1)
+    void update(OpT const op) {
+        lazy[1] = Op::f(lazy[1], op);
+    }
+// private:
+
+    void re_calc(int l, int r) {
+        r--;
+        l >>= 1; r >>= 1;
+        while (l > 0) {
+            for (int i = l; i <= r; ++i) v[i] = M::f(v[2 * i], v[2 * i + 1]);
+            l >>= 1; r >>= 1;
+        }
+    }
+
+    vector<MT> v;
+    vector<OpT> lazy;
+    int n;
+    int depth;
+};
+
+// End of template.
+
+int minint(int a, int b) { return min(a, b); }
+
+template<>
+int Monoid<int, minint>::pow(int base, int exp) {
+    return base;
+}
+
+template<>
+const int Monoid<int, minint>::unit(INF);
 
 int main(){
     cout << fixed << setprecision(15);
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    string s;
+    cin >> s;
+    n = s.length();
+
+    using M = Monoid<int, minint>;
+    using ST = LazySegmentTree<M>;
+
+    ST st(n + 1);
+    st.set(n, 0);
+
+
     return 0;
 }
+
 
