67179ed93cf88b9214e7dfa8a7d5b095e828986a
Tsubasa Munekata <tsubasa.afe@gmail.com>
2019-12-08 03:46:18 +0900

    Add mod.test.cpp

---
+++ 0templates/mod.test.cpp
@@ -0,0 +1,98 @@
+/*                         _
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> \"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+
+                  Please give me AC.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <complex>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <random>
+
+using namespace std;
+
+using int64 = long long;
+using uint64 = unsigned long long;
+using vi = vector<int>;
+using vl = vector<int64>;
+using pii = pair<int, int>;
+using pll = pair<int64, int64>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define print(x) cout << (x) << '\n'
+#define print2(x, y) cout << (x) << ' ' << (y) << '\n'
+#define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
+#define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#define debug(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
+// constant
+const int INF = (1<<30) - 1;
+const int64 INF64 = (1LL<<62) - 1;
+
+#include "mod.cpp"
+
+// End of template.
+
+#define ASSERT(expr) if (!(expr)) return 1;
+
+mt19937 mt;
+
+int randint(int a, int b) {
+    return uniform_int_distribution<int>(a, b - 1)(mt);
+}
+
+int main(){
+    cout << fixed << setprecision(15);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    constexpr int64 mod = 1e9 + 7;
+    using M = Mod<int64, mod>;
+
+    cout << "Mod::inv";
+    rep(i, 1000000) {
+        M a = randint(1, mod);
+        M inv = a.inv();
+        ASSERT((a * inv).value == 1);
+    }
+
+    return 0;
+}
+
