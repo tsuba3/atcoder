9439c5f8190c5e99e95563bb307cdc0da8fb231a
Tsubasa Munekata <tsubasa.afe@gmail.com>
2019-12-08 16:23:40 +0900

    #/usr/local/bin/zsh を #/bin/zsh に変更

--- 0templates/check.sh
+++ 0templates/check.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/zsh
+#!/bin/zsh
 
 IN=${IN:-"in"}
 OUT=${OUT:-"out"}
--- 0templates/test-swag.sh
+++ 0templates/test-swag.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/zsh
+#!/bin/zsh
 
 
 g++ -std=c++14 -O2 swag.test.cpp && IN="swag-testcase/input" OUT="swag-testcase/output" ./check.sh && rm -f a.out
