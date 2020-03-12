de1cdc843e3b80af983a2d8fdb3edbc760ff093e
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-03-12 10:05:26 +0900

    Add writeui to fastio.cpp

--- 0templates/fastio.cpp
+++ 0templates/fastio.cpp
@@ -1,7 +1,7 @@
 // begin fast io
 
-char buf[10000000];
-int bufi = 0;
+char buf[10000000], OUT[10000000];
+int bufi = 0, outi = 0;
 
 template<typename T>
 void readui(T &n) {
@@ -53,6 +53,83 @@ void readline(string &s) {
     while (buf[bufi++] != '\n') {}
     s = string(buf + begin, buf + bufi);
 }
+
+const char digit_pairs[] = {
+  "00010203040506070809"
+  "10111213141516171819"
+  "20212223242526272829"
+  "30313233343536373839"
+  "40414243444546474849"
+  "50515253545556575859"
+  "60616263646566676869"
+  "70717273747576777879"
+  "80818283848586878889"
+  "90919293949596979899"
+};
+
+template<typename T>
+void writeui(T n) {
+    if (n == 0) OUT[outi++] = '0';
+    int size;
+    if (n >= 1e12) {
+        if (n >= 1e16) {
+            if (n >= 1e18) {
+                if (n >= 1e19) size = 20;
+                else size = 19;
+            } else {
+                if (n >= 1e17) size = 18;
+                else size = 17;
+            }
+        } else {
+            if (n >= 1e14) {
+                if (n >= 1e15) size = 16;
+                else size = 15;
+            } else {
+                if (n >= 1e13) size = 14;
+                else size = 13;
+            }
+        }
+    } else if (n >= 1e4) {
+        if (n >= 1e8) {
+            if (n >= 1e10) {
+                if (n >= 1e11) size = 12;
+                else size = 11;
+            } else {
+                if (n >= 1e9) size = 10;
+                else size = 9;
+            }
+        } else {
+            if (n >= 1e6) {
+                if (n >= 1e7) size = 8;
+                else size = 7;
+            } else {
+                if (n >= 1e5) size = 6;
+                else size = 5;
+            }
+        }
+    } else {
+        if (n >= 1e2) {
+            if (n >= 1e3) size = 4;
+            else size = 3;
+        } else {
+            if (n >= 1e1) size = 2;
+            else size = 1;
+        }
+    }
+    char *c = OUT + outi + size;
+    outi += size;
+    while (n >= 100) {
+        int x = n % 100;
+        n /= 100;
+        *--c = digit_pairs[2 * x + 1];
+        *--c = digit_pairs[2 * x];
+    }
+    while (n > 0) {
+        *--c = '0' + (n % 10);
+        n /= 10;
+    }
+}
+
 
 // end fast io
 
