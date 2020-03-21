905335b4fce6e87beae5ec95e256a7d5980ab41d
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-03-21 20:45:07 +0900

    panasonic2020

---
+++ panasonic2020/e.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+hatena = ord('?') # 63
+
+def parse_input(s):
+    return np.array(list(map(ord, s)), dtype='int8')
+
+def calc(a, b):
+    a = 
+
+if __name__ == '__main__':
+    a = parse_input(input())
+    b = parse_input(input())
+    c = parse_input(input())
+
+
