64ffa1005bd0afea1c5e3e4b205931c23d9d783a
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-03-22 12:01:05 +0900

    AGC043

---
+++ agc043/b.cpp
@@ -0,0 +1,139 @@
+/*                         _
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> \"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+
+                  Please give me AC.
+*/
+
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <complex>
+#include <bitset>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <chrono>
+#include <random>
+#include <wmmintrin.h>
+
+using namespace std;
+
+using int64 = long long;
+using uint64 = unsigned long long;
+using vi = vector<int>;
+using vl = vector<int64>;
+using pii = pair<int, int>;
+using pll = pair<int64, int64>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define print(x) cout << (x) << '\n'
+#define print2(x, y) cout << (x) << ' ' << (y) << '\n'
+#define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
+#define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#define debug(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
+// constant
+const int INF = (1<<30) - 1;
+const int64 INF64 = (1LL<<62) - 1;
+
+template<typename T> T gcd(T a, T b) {
+    if (a < b) return gcd(b, a);
+    T r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+template<typename T> T lcm(const T a, const T b) {
+    return a / gcd(a, b) * b;
+}
+
+template<typename T> bool chmin(T& a, const T& b) {
+    if (a > b) return a = b, true; else return false;
+}
+
+template<typename T> bool chmax(T& a, const T& b) {
+    if (a < b) return a = b, true; else return false;
+}
+
+// End of template.
+// begin fast io
+
+char buf[1010000];
+int bufi = 0, outi = 0;
+
+template<typename T>
+void readui(T &n) {
+    n = 0;
+    int c = buf[bufi++];
+    for (; '0' <= c && c <= '9'; c = buf[bufi++])
+        n = 10 * n + c - '0';
+}
+
+// end fast io
+
+char Cmod2(int n, int r) {
+    return n == (r | (n - r)) ? 3 : 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin.read(buf, sizeof buf); // 注意: ./a.out < in か pbp | ./a.out で入力すること
+
+    int n;
+    readui(n);
+    char *a = buf + bufi;
+
+    char x = 0, y = 0;
+    rep(i, n) a[i] -= '1', x ^= Cmod2(n - 1, i) & a[i], y |= a[i];
+    if (x & 1) {
+        print(1);
+    } else if (y & 1) {
+        print(0);
+    } else if (x & 2) {
+        print(2);
+    } else {
+        print(0);
+    }
+    return 0;
+}
+
---
+++ agc043/r.py
@@ -0,0 +1,3 @@
+import random
+print(''.join([str(random.randint(1, 3)) for _ in range(200)]))
+
