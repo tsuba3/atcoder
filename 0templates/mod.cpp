3273cdd8d7da38b6a67e72298baadfd0a950a570
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-04-18 14:20:31 +0900

    Mod に constexpr をつける

--- 0templates/mod.cpp
+++ 0templates/mod.cpp
@@ -5,17 +5,18 @@ public:
     using Self = Mod<T, mod_, is_prime>;
     T value;
     static constexpr T mod = mod_;
-    Mod(T x) { value = x % mod; }
-    explicit operator T&() { return value; }
-    Self operator +(Self const x) const { return (value + x.value) % mod; }
-    Self operator *(Self const x) const { return (value * x.value) % mod; }
-    Self operator -(Self const x) const { return (mod + value - x.value) % mod; }
-    Self operator /(Self const x) const { return (value * x.inv().value) % mod; }
-    Self operator +=(Self const x) { return value = (value + x.value) % mod; }
-    Self operator *=(Self const x) { return value = (value * x.value) % mod; }
-    Self operator -=(Self const x) { return value = (mod + value - x.value) % mod; }
-    Self operator /=(Self const x) { return value = (value * x.inv().value) % mod; }
-    Self inv() const {
+    constexpr Mod() : value() {}
+    constexpr Mod(T x) : value() { value = x % mod; }
+    explicit constexpr operator T&() { return value; }
+    constexpr Self operator +(Self const x) const { return (value + x.value) % mod; }
+    constexpr Self operator *(Self const x) const { return (value * x.value) % mod; }
+    constexpr Self operator -(Self const x) const { return (mod + value - x.value) % mod; }
+    constexpr Self operator /(Self const x) const { return (value * x.inv().value) % mod; }
+    constexpr Self operator +=(Self const x) { return value = (value + x.value) % mod; }
+    constexpr Self operator *=(Self const x) { return value = (value * x.value) % mod; }
+    constexpr Self operator -=(Self const x) { return value = (mod + value - x.value) % mod; }
+    constexpr Self operator /=(Self const x) { return value = (value * x.inv().value) % mod; }
+    constexpr Self inv() const {
         T a = value, b = mod, u = 1, v = 0;
         while (b) {
             T t = a / b;
@@ -26,7 +27,7 @@ public:
         if (u < 0) u += mod;
         return u;
     }
-    Self pow(int e) const {
+    constexpr Self pow(int e) const {
         if (e < 0) return inv().pow(-e);
         if (is_prime) e %= mod - 1;
         Self base = value;
