ef3b5746972a553f6b3ff4f49930bdf6bbd99ebe
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-04-19 16:41:34 +0900

    ABC161 F

--- abc161/f.cpp
+++ abc161/f.cpp
@@ -92,38 +92,11 @@ template<typename T> bool chmax(T& a, const T& b) {
 
 // End of template.
 
-// Begin of sieve
-// コンパイル時エラトステネスの篩
-
-constexpr size_t primes_limit[] = {2,2,2,4,6,11,18,31,54,97,172,309,564,1028,1900,3512,6542,12251,23000,43390,82025,155611};
-
-template<size_t N>
-struct Sieve {
-    uint8_t flag[N / 8 + 2];
-    int primes[primes_limit[8*sizeof(int) - __builtin_clz(N)]]; // N 以下の素数の配列
-    int sz; // N 以下の素数の数
-    constexpr Sieve() : flag(), primes(), sz(0) {
-        rep(i, sizeof flag) flag[i] = 0b10101010;
-        flag[0] = 0b10101100;
-        for (int i = 3; i <= sqrt(N); i += 2) if (flag[i>>3] >> (i&7) & 1) {
-            for (int p = i * i; p <= N; p += i)
-                flag[p >> 3] &= ~(1 << (p&7));
-        }
-        int j = 0;
-        rep(i, N + 1) if (flag[i >> 3] >> (i & 7) & 1) primes[j++] = i;
-        sz = j;
-    }
-    template<typename T> bool test(T p) const { // p が素数かどうかを返す。p は N 2乗以下であること。
-        if (p <= N) return flag[p >> 3] >> (p & 7) & 1;
-        for (int i = 0; primes[i] <= sqrt(p) + 1; ++i) {
-            if (p % primes[i] == 0) return false;
-        }
-        return true;
-    }
-};
-// End of sieve
-
-constexpr Sieve<1000000> S;
+bool test(int64 n, int64 x) {
+    if (x == 1) return false;
+    while (n % x == 0) n /= x;
+    return n % x == 1;
+}
 
 int main() {
     cout << fixed << setprecision(15);
@@ -133,6 +106,23 @@ int main() {
 
     int64 n;
     cin >> n;
+    int64 ans = 0;
+    int64 i;
+    for (i = 2; i * i < n; ++i) if (n % i == 0) {
+        ans += test(n, i);
+        ans += test(n, n / i);
+    }
+    if (i * i == n) ans += test(n, i);
+    ans += test(n, n);
+
+    for (i = 2; i * i < n - 1; ++i) if ((n - 1) % i == 0) {
+        ans += test(n, i);
+        ans += test(n, (n - 1) / i);
+    }
+    if (i * i == n - 1) ans += test(n, i);
+    ans += test(n, n - 1);
+
+    print(ans);
 
     return 0;
 }
