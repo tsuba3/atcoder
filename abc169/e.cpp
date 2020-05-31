027c8856c365a1291e2b219886cc13c3c38c5b1d
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-06-01 00:56:15 +0900

    ABC169

---
+++ abc169/e.cpp
@@ -0,0 +1,149 @@
+/*                         _
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> \"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+
+                  Please give me AC.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <complex>
+#include <bitset>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+using int64 = long long;
+using uint64 = unsigned long long;
+using vi = vector<int>;
+using vl = vector<int64>;
+using pii = pair<int, int>;
+using pll = pair<int64, int64>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define print(x) cout << (x) << '\n'
+#define print2(x, y) cout << (x) << ' ' << (y) << '\n'
+#define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
+#define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#define debug(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
+// constant
+const int INF = (1<<30) - 1;
+const int64 INF64 = (1LL<<62) - 1;
+
+template<typename T> T gcd(T a, T b) {
+    if (a < b) return gcd(b, a);
+    T r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+template<typename T> T lcm(const T a, const T b) {
+    return a / gcd(a, b) * b;
+}
+
+template<typename T> bool chmin(T& a, const T& b) {
+    if (a > b) return a = b, true; else return false;
+}
+
+template<typename T> bool chmax(T& a, const T& b) {
+    if (a < b) return a = b, true; else return false;
+}
+
+// End of template.
+template<typename T>
+struct Compress {
+    vector<T> v;
+    constexpr Compress() : v() {}
+    constexpr Compress(vector<T>&& v) : v(v) {
+        build();
+    }
+    static constexpr Compress<T> with_sorted(vector<T>&& v) {
+        return Compress<T>{v};
+    }
+    void build() {
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+    }
+    int compress(const T x) const {
+        return lower_bound(v.begin(), v.end(), x) - v.begin();
+    }
+    const T& restore(const int i) const { return v[i]; }
+    int operator()(const T&& x) const { return compress(x); }
+    const T& operator[](const int i) const { return restore(i); }
+};
+
+int main() {
+    cout << fixed << setprecision(15);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // cin.read(buf, sizeof buf); // 注意: ./a.out < in か pbp | ./a.out で入力すること
+
+    int n;
+    vector<pii> a;
+    cin >> n;
+    a.resize(n);
+    rep(i, n) cin >> a[i].first >> a[i].second;
+
+    if (n % 2 == 1) {
+        // odd
+        sort(a.begin(),  a.end(), [](pii lhs, pii rhs) { return lhs.second < rhs.second; });
+        const int h = a[a.size() / 2].second;
+        sort(a.begin(),  a.end(), [](pii lhs, pii rhs) { return lhs.first < rhs.first; });
+        const int l = a[a.size() / 2].first;
+
+        print(h - l + 1);
+    } else {
+        //even
+        sort(a.begin(),  a.end(), [](pii lhs, pii rhs) { return lhs.second < rhs.second; });
+        const int h2 = a[a.size() / 2].second + a[a.size() / 2 - 1].second;
+        sort(a.begin(),  a.end(), [](pii lhs, pii rhs) { return lhs.first < rhs.first; });
+        const int l2 = a[a.size() / 2].first + a[a.size() / 2 - 1].first;
+
+        print(h2 - l2 + 1);
+    }
+
+
+    return 0;
+}
+
