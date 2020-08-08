3aac7834f7cbb6cce6e058a1cf9570c5594b3556
Tsubasa Munekata <tsubasa.afe@gmail.com>
2020-08-08 19:58:38 +0900

    abc164

--- abc164/b.cpp
+++ abc164/b.cpp
@@ -60,10 +60,18 @@ using pll = pair<int64, int64>;
 #define print2(x, y) cout << (x) << ' ' << (y) << '\n'
 #define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
 #define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#define dbg(v)
+#else
 #define debug(x) cerr << #x << ": " << (x) << '\n'
 #define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
 #define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
 #define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
+#endif
+
 // constant
 const int INF = (1<<30) - 1;
 const int64 INF64 = (1LL<<62) - 1;
@@ -91,29 +99,208 @@ template<typename T> bool chmax(T& a, const T& b) {
 }
 
 // End of template.
+// begin fast io
+template <typename IStream = decltype(cin), typename OStream = decltype(cout)>
+struct FastIO {
+    char *buf, *OUT, *out;
+    OStream& os;
 
-int main() {
-    cout << fixed << setprecision(15);
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // cin.read(buf, sizeof buf); // 注意: ./a.out < in か pbp | ./a.out で入力すること
+    FastIO(size_t input_size = 1e7, size_t output_size = 1e7): os(cout) {
+        buf = new char[input_size];
+        OUT = new char[output_size];
+        out = OUT;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cin.read(buf, input_size);
+    }
 
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
+    FastIO(IStream& in, OStream& out, size_t input_size = 1e7, size_t output_size = 1e7): os(out) {
+        buf = new char[input_size];
+        OUT = new char[output_size];
+        this-> out = OUT;
+        in.read(buf, input_size);
+    }
 
-    while (true) {
-        a -= d;
-        c -= b;
-        if (c <= 0) {
-            print("Yes");
-            return 0;
+    ~FastIO() {
+        os.write(OUT, out - OUT);
+    }
+
+    char readc() {
+        return *buf++;
+    }
+
+    template<typename T>
+    void readui(T &n) {
+        n = 0;
+        char c = readc();
+        for (; '0' <= c && c <= '9'; c = readc())
+            n = 10 * n + c - '0';
+    }
+
+    template<typename T>
+    void readi(T &n) {
+        bool negative = false;
+        if (*buf == '-') negative = true, buf++;
+        readui(n);
+        if (negative) n = -n;
+    }
+
+    template<typename T>
+    void readuf(T &x) {
+        x = 0;
+        T y = 0;
+        int z = 0;
+        char c = readc();
+        for (; '0' <= c && c <= '9'; c = readc())
+            x = 10 * x + c - '0';
+        if (buf[-1] == '.')
+            for (; '0' <= c && c <= '9'; c = readc(), ++z)
+                y = 10 * y + c - '0';
+        x += y / pow(10, z);
+    }
+
+    template<typename T>
+    void readf(T &x) {
+        bool negative = false;
+        if (*buf == '-') negative = true, buf++;
+        readuf(x);
+        if (negative) x = -x;
+    }
+
+    string_view reads() {
+        const char *begin = buf;
+        char c;
+        while (c = readc(), c != ' ' && c != '\n') {}
+        return string_view(begin, buf - begin - 1);
+    }
+
+    string_view readline() {
+        const char *begin = buf;
+        while (readc() != '\n') {}
+        return string_view(begin, buf - begin - 1);
+    }
+
+    void writec(const char c) {
+        *out++ = c;
+    }
+
+    void writes(const string& s) {
+        memcpy(out, s.c_str(), s.size());
+        out += s.size();
+    }
+
+    void writes(const string_view& s) {
+        memcpy(out, s.data(), s.size());
+        out += s.size();
+    }
+
+    constexpr static char digit_pairs[] = {
+            "00010203040506070809"
+            "10111213141516171819"
+            "20212223242526272829"
+            "30313233343536373839"
+            "40414243444546474849"
+            "50515253545556575859"
+            "60616263646566676869"
+            "70717273747576777879"
+            "80818283848586878889"
+            "90919293949596979899"
+    };
+
+    template<typename T>
+    void writeui(T n) {
+        if (n == 0) { writec('0'); return; }
+        int size;
+        if (n >= 1e12) {
+            if (n >= 1e16) {
+                if (n >= 1e18) {
+                    if (n >= 1e19) size = 20;
+                    else size = 19;
+                } else {
+                    if (n >= 1e17) size = 18;
+                    else size = 17;
+                }
+            } else {
+                if (n >= 1e14) {
+                    if (n >= 1e15) size = 16;
+                    else size = 15;
+                } else {
+                    if (n >= 1e13) size = 14;
+                    else size = 13;
+                }
+            }
+        } else if (n >= 1e4) {
+            if (n >= 1e8) {
+                if (n >= 1e10) {
+                    if (n >= 1e11) size = 12;
+                    else size = 11;
+                } else {
+                    if (n >= 1e9) size = 10;
+                    else size = 9;
+                }
+            } else {
+                if (n >= 1e6) {
+                    if (n >= 1e7) size = 8;
+                    else size = 7;
+                } else {
+                    if (n >= 1e5) size = 6;
+                    else size = 5;
+                }
+            }
+        } else {
+            if (n >= 1e2) {
+                if (n >= 1e3) size = 4;
+                else size = 3;
+            } else {
+                if (n >= 1e1) size = 2;
+                else size = 1;
+            }
         }
-        if (a <= 0) {
-            print("No");
-            return 0;
+        char *c = out + size;
+        out += size;
+        while (n >= 100) {
+            int x = n % 100;
+            n /= 100;
+            *--c = digit_pairs[2 * x + 1];
+            *--c = digit_pairs[2 * x];
+        }
+        while (n > 0) {
+            *--c = '0' + (n % 10);
+            n /= 10;
         }
     }
+
+    template<typename T>
+    void writei(T n) {
+        if (n < 0) {
+            writec('-');
+            writeui(-n);
+        } else {
+            writeui(n);
+        }
+    }
+};
+// end fast io
+
+int main() {
+    FastIO io;
+
+    int a, b, c, d;
+    io.readui(a), io.readui(b), io.readui(c), io.readui(d);
+
+    int t = (c + b - 1) / b;
+    int s = (a + d - 1) / d;
+
+    if (t <= s) io.writes(string("Yes\n")); else io.writes(string("No\n"));
 
     return 0;
 }
 
+/*
+ _
+| |__   ___ _ __  _ __
+| '_ \ / __| '_ \| '_ \
+| |_) | (__| |_) | |_) |
+|_.__(_)___| .__/| .__/
+           |_|   |_|
+*/
--- abc164/e.cpp
+++ abc164/e.cpp
@@ -60,16 +60,23 @@ using pll = pair<int64, int64>;
 #define print2(x, y) cout << (x) << ' ' << (y) << '\n'
 #define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
 #define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#define dbg(v)
+#else
 #define debug(x) cerr << #x << ": " << (x) << '\n'
 #define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
 #define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
 #define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
-// constant
-const int INF = (1 << 30) - 1;
-const int64 INF64 = (1LL << 62) - 1;
+#endif
 
-template<typename T>
-T gcd(T a, T b) {
+// constant
+const int INF = (1<<30) - 1;
+const int64 INF64 = (1LL<<62) - 1;
+
+template<typename T> T gcd(T a, T b) {
     if (a < b) return gcd(b, a);
     T r;
     while ((r = a % b)) {
@@ -79,94 +86,271 @@ T gcd(T a, T b) {
     return b;
 }
 
-template<typename T>
-T lcm(const T a, const T b) {
+template<typename T> T lcm(const T a, const T b) {
     return a / gcd(a, b) * b;
 }
 
-template<typename T>
-bool chmin(T &a, const T &b) {
+template<typename T> bool chmin(T& a, const T& b) {
     if (a > b) return a = b, true; else return false;
 }
 
-template<typename T>
-bool chmax(T &a, const T &b) {
+template<typename T> bool chmax(T& a, const T& b) {
     if (a < b) return a = b, true; else return false;
 }
 
 // End of template.
+// begin fast io
+template <typename IStream = decltype(cin), typename OStream = decltype(cout)>
+struct FastIO {
+    char *buf, *OUT, *out;
+    OStream& os;
 
-struct V {
-    int i;
-    int ginka;
-};
+    FastIO(size_t input_size = 1e7, size_t output_size = 1e7): os(cout) {
+        buf = new char[input_size];
+        OUT = new char[output_size];
+        out = OUT;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cin.read(buf, input_size);
+    }
 
-constexpr int fare_max = 2500;
+    FastIO(IStream& in, OStream& out, size_t input_size = 1e7, size_t output_size = 1e7): os(out) {
+        buf = new char[input_size];
+        OUT = new char[output_size];
+        this-> out = OUT;
+        in.read(buf, input_size);
+    }
 
-template <typename T> struct A {
-    T a[52][fare_max];
-    T& operator[](V v) {
-        return a[v.i][v.ginka];
+    ~FastIO() {
+        os.write(OUT, out - OUT);
+    }
+
+    char readc() {
+        return *buf++;
+    }
+
+    template<typename T>
+    void readui(T &n) {
+        n = 0;
+        char c = readc();
+        for (; '0' <= c && c <= '9'; c = readc())
+            n = 10 * n + c - '0';
+    }
+
+    template<typename T>
+    void readi(T &n) {
+        bool negative = false;
+        if (*buf == '-') negative = true, buf++;
+        readui(n);
+        if (negative) n = -n;
+    }
+
+    template<typename T>
+    void readuf(T &x) {
+        x = 0;
+        T y = 0;
+        int z = 0;
+        char c = readc();
+        for (; '0' <= c && c <= '9'; c = readc())
+            x = 10 * x + c - '0';
+        if (buf[-1] == '.')
+            for (; '0' <= c && c <= '9'; c = readc(), ++z)
+                y = 10 * y + c - '0';
+        x += y / pow(10, z);
+    }
+
+    template<typename T>
+    void readf(T &x) {
+        bool negative = false;
+        if (*buf == '-') negative = true, buf++;
+        readuf(x);
+        if (negative) x = -x;
+    }
+
+    string_view reads() {
+        const char *begin = buf;
+        char c;
+        while (c = readc(), c >= '!') {}
+        return string_view(begin, buf - begin - 1);
+    }
+
+    string_view readline() {
+        const char *begin = buf;
+        while (readc() != '\n') {}
+        return string_view(begin, buf - begin - 1);
+    }
+
+    void writec(const char c) {
+        *out++ = c;
+    }
+
+    void writes(const string& s) {
+        memcpy(out, s.c_str(), s.size());
+        out += s.size();
+    }
+
+    void writes(const string_view& s) {
+        memcpy(out, s.data(), s.size());
+        out += s.size();
+    }
+
+    constexpr static char digit_pairs[] = {
+            "00010203040506070809"
+            "10111213141516171819"
+            "20212223242526272829"
+            "30313233343536373839"
+            "40414243444546474849"
+            "50515253545556575859"
+            "60616263646566676869"
+            "70717273747576777879"
+            "80818283848586878889"
+            "90919293949596979899"
+    };
+
+    template<typename T>
+    void writeui(T n) {
+        if (n == 0) { writec('0'); return; }
+        int size;
+        if (n >= 1e12) {
+            if (n >= 1e16) {
+                if (n >= 1e18) {
+                    if (n >= 1e19) size = 20;
+                    else size = 19;
+                } else {
+                    if (n >= 1e17) size = 18;
+                    else size = 17;
+                }
+            } else {
+                if (n >= 1e14) {
+                    if (n >= 1e15) size = 16;
+                    else size = 15;
+                } else {
+                    if (n >= 1e13) size = 14;
+                    else size = 13;
+                }
+            }
+        } else if (n >= 1e4) {
+            if (n >= 1e8) {
+                if (n >= 1e10) {
+                    if (n >= 1e11) size = 12;
+                    else size = 11;
+                } else {
+                    if (n >= 1e9) size = 10;
+                    else size = 9;
+                }
+            } else {
+                if (n >= 1e6) {
+                    if (n >= 1e7) size = 8;
+                    else size = 7;
+                } else {
+                    if (n >= 1e5) size = 6;
+                    else size = 5;
+                }
+            }
+        } else {
+            if (n >= 1e2) {
+                if (n >= 1e3) size = 4;
+                else size = 3;
+            } else {
+                if (n >= 1e1) size = 2;
+                else size = 1;
+            }
+        }
+        char *c = out + size;
+        out += size;
+        while (n >= 100) {
+            int x = n % 100;
+            n /= 100;
+            *--c = digit_pairs[2 * x + 1];
+            *--c = digit_pairs[2 * x];
+        }
+        while (n > 0) {
+            *--c = '0' + (n % 10);
+            n /= 10;
+        }
+    }
+
+    template<typename T>
+    void writei(T n) {
+        if (n < 0) {
+            writec('-');
+            writeui(-n);
+        } else {
+            writeui(n);
+        }
     }
 };
+// end fast io
 
-struct Edge {
-    V to;
-    int64 time;
+struct V {
+    int x;
+    V(int v, int s) : x(2501 * v + clamp(s, 0, 2500)) {}
+    operator int() {
+        return x;
+    }
+    int v() { return x / 2501; }
+    int s() { return x % 2501; }
 };
 
-A<int64> dp;
-A<bool> done;
-A<vector<Edge>> vec;
-
 int main() {
-    cout << fixed << setprecision(15);
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // cin.read(buf, sizeof buf); // 注意: ./a.out < in か pbp | ./a.out で入力すること
+    FastIO io;
 
-    int n, m, s0;
-    cin >> n >> m >> s0;
-    rep(i, m) {
+    int n, m, s;
+    io.readui(n), io.readui(m), io.readui(s);
+    s = clamp(s, 0, 2500);
+    vector<pair<V, int>> vec[50*2502];
+    vector<int64> dp(n * 2501, INF64);
+
+    rep(mi, m) {
         int u, v, a, b;
-        cin >> u >> v >> a >> b;
+        io.readui(u), io.readui(v), io.readui(a), io.readui(b);
         u--, v--;
-        for (int ginka = a; ginka <= fare_max; ++ginka) vec[{u, ginka}].emplace_back(Edge{{v, ginka - a}, b});
-        for (int ginka = a; ginka <= fare_max; ++ginka) vec[{v, ginka}].emplace_back(Edge{{u, ginka - a}, b});
+        rep(i, 2501 - a) {
+            vec[V(u, i + a)].emplace_back(V(v, i), b);
+            vec[V(v, i + a)].emplace_back(V(u, i), b);
+        }
     }
 
     rep(i, n) {
         int c, d;
-        cin >> c >> d;
-        for (int ginka = 0; ginka < fare_max; ++ginka) vec[{i, ginka}].emplace_back(Edge{{i, min(ginka + c, fare_max)}, d});
+        io.readui(c), io.readui(d);
+        rep(j, 2500) vec[V(i, j)].emplace_back(V(i, clamp(j + c, 0, 2500)), d);
     }
 
-    for (int i = 0; i < n; ++i) for (int fare = 0; fare <= fare_max; ++fare) dp[{i, fare}] = INF64;
-    auto comp = [](V lhs, V rhs) {
-        return dp[lhs] < dp[rhs];
+    using P = pair<int64, V>;
+    struct comp {
+        bool operator()(P l, P r) {
+            return l.first > r.first;
+        }
     };
-    priority_queue<V, vector<V>, decltype(comp)> queue(comp);
-    dp[{0, min(fare_max, s0)}] = 0;
-    queue.emplace(V{0, min(fare_max, s0)});
-
-    while (!queue.empty()) {
-        V v = queue.top();
-        queue.pop();
-        if (done[v]) continue;
-        done[v] = true;
-        for (auto e : vec[v]) {
-            if (chmin(dp[e.to], dp[v] + e.time)) {
-                queue.emplace(e.to);
+    priority_queue<P, vector<P>, comp> que;
+    dp[V(0, s)] = 0;
+    que.emplace(0, V(0, s));
+    while (!que.empty()) {
+        auto [_, v] = que.top();
+        que.pop();
+        if (dp[v] < _) continue;
+        for (auto k : vec[v]) {
+            if (chmin(dp[k.first], dp[v] + k.second)) {
+                que.emplace(dp[k.first.x], k.first);
             }
         }
     }
 
     for (int i = 1; i < n; ++i) {
-        int64 t = INF64;
-        for (int fare = 0; fare <= fare_max; ++fare) chmin(t, dp[{i, fare}]);
-        print(t);
+        int64 ans = INF64;
+        rep(j, 2500) chmin(ans, dp[V(i, j)]);
+        io.writeui(ans);
+        io.writec('\n');
     }
 
     return 0;
 }
 
+/*
+  ___   ___ _ __  _ __
+ / _ \ / __| '_ \| '_ \
+|  __/| (__| |_) | |_) |
+ \___(_)___| .__/| .__/
+           |_|   |_|
+*/
