d397e98686cae982b431c098ec288d2994c33152
Tsubasa Munekata <tsubasa.afe@gmail.com>
2021-02-20 22:40:24 +0900

    ABC192

---
+++ arc112/c.cpp
@@ -0,0 +1,164 @@
+/*                         _
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    ____/`---'\____
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /  _||||| -:- |||||_  \
+                |   | \\\  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> \"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+
+                  Please give me AC.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <complex>
+#include <bitset>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+using int64 = long long;
+using uint64 = unsigned long long;
+using vi = vector<int>;
+using vl = vector<int64>;
+using pii = pair<int, int>;
+using pll = pair<int64, int64>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define print(x) cout << (x) << '\n'
+#define print2(x, y) cout << (x) << ' ' << (y) << '\n'
+#define print3(x, y, z) cout << (x) << ' ' << (y) << ' ' << (z) << '\n'
+#define printn(v) rep(i, (v).size() - 1) cout << (v)[i] << ' '; cout << (v)[n - 1] << '\n';
+#ifdef ONLINE_JUDGE
+#define debug(x)
+#define debug2(x, y)
+#define debug3(x, y, z)
+#define dbg(v)
+#else
+#define debug(x) cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v) for (size_t _ = 0; _ < v.size(); ++_){cerr << #v << "[" << _ << "] : " << v[_] << '\n';}
+#endif
+
+// constant
+const int INF = (1<<30) - 1;
+const int64 INF64 = (1LL<<62) - 1;
+
+template<typename T> T gcd(T a, T b) {
+    if (a < b) return gcd(b, a);
+    T r;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+template<typename T> T lcm(const T a, const T b) {
+    return a / gcd(a, b) * b;
+}
+
+template<typename S, typename T> bool chmin(S& a, const T& b) {
+    if (a > b) return a = b, true; else return false;
+}
+
+template<typename S, typename T> bool chmax(S& a, const T& b) {
+    if (a < b) return a = b, true; else return false;
+}
+
+// End of template.
+
+int n, p[100100];
+vector<vector<int>> vec;
+vector<int> d;
+
+void dfs(int u) {
+    for (int v : vec[u]) {
+        dfs(v);
+        d[u] = d[v] + 1;
+    }
+}
+
+void dfs2(int u) {
+
+}
+
+int main() {
+    cout << fixed << setprecision(15);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cerr.tie(nullptr);
+
+
+    cin >> n;
+    p[0] = 0;
+    rep(i, n - 1) {
+        cin >> p[i + 1];
+        p[i + 1]--;
+    }
+
+    vec.resize(n);
+    rep(i, n - 1) {
+        vec[p[i + 1]].push_back(i + 1);
+    }
+
+    d.resize(n, 1);
+    dfs(0);
+
+    vector<int> x;
+    for (int i : vec[0]) {
+        x.push_back(d[i] + 1);
+    }
+
+    sort(x.begin(), x.end());
+
+    if (x.size() == 1) {
+        print(1);
+    } else {
+        print(x[x.size() - 2] + 1);
+    }
+
+
+    return 0;
+}
+
+/*
+  ___   ___ _ __  _ __
+ / __| / __| '_ \| '_ \
+| (__ | (__| |_) | |_) |
+ \___(_)___| .__/| .__/
+           |_|   |_|
+*/
